9fd5509fdee4fc9cb95a346b0bda141f92db2d57
이승철 <thankfather@naver.com>
2020-01-29 18:29:16 +0900

    remove unnecessary part

--- recommend-algorithm/recommendation_model.py
+++ recommend-algorithm/recommendation_model.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import numpy as np
 
 
 class Model:
@@ -7,21 +6,25 @@ class Model:
         self.nu = rated_matrix.shape[0]
         self.ni = rated_matrix.shape[1]
         self.nf = nf
-        self.X = tf.cast(np.random.rand(self.nu, nf) * 0.01, dtype=tf.float32)
-        self.Y = tf.cast(np.random.rand(self.ni, nf) * 0.01, dtype=tf.float32)
-        self.P = np.copy(rated_matrix)
-        self.P[self.P > 0] = 1
+
+        self.X = tf.Variable(tf.random.normal(shape=(self.nu, nf), dtype=tf.float32))
+        self.X.assign(tf.math.multiply(self.X, 0.01))
+        self.Y = tf.Variable(tf.random.normal(shape=(self.ni, nf), dtype=tf.float32))
+        self.Y.assign(tf.math.multiply(self.Y, 0.01))
+
         self.C = 1 + alpha * rated_matrix
         self.lambda_var = 40
+        self.P = tf.Variable(tf.identity(rated_matrix))
+        for i in range(self.nu):
+            self.P[i].assign(tf.map_fn(lambda x: 1 if x > 0 else x, self.P[i]))
 
     def loss_function(self, r_lambda=40):
         self.lambda_var = r_lambda
-        xTy = tf.matmul(tf.transpose(self.X), self.Y)
-        predicted_error = tf.square(self.P - xTy)
-        confidence_error = tf.reduce_sum(tf.matmul(self.C, predicted_error))
-        regularization = r_lambda * tf.reduce_sum(tf.square(self.X) + tf.square(tf.square(self.Y)))
-        total_loss = confidence_error + regularization
-        return tf.reduce_sum(predicted_error), confidence_error, regularization, total_loss
+        predict = self.predict()
+        predicted_error = tf.reduce_sum(self.C * tf.square(self.P - predict))
+        regularization = r_lambda * (tf.reduce_sum(tf.square(self.X)) + tf.reduce_sum(tf.square(self.Y)))
+        total_loss = predicted_error + regularization
+        return tf.reduce_sum(predicted_error), regularization, total_loss
 
     def optimize_user(self):
         yT = tf.transpose(self.Y)
@@ -30,9 +33,9 @@ class Model:
             Cu = tf.linalg.diag(self.C[u])
             yTCuY = yTy + tf.matmul(tf.matmul(yT, Cu - tf.eye(Cu.shape[1])), self.Y)
             lI = self.lambda_var * tf.eye(self.nf)
-            print(self.P[u])
-            yTCuPu = tf.matmul(tf.matmul(yT, Cu), self.P[u])
-            self.X[u] = tf.matmul(tf.linalg.inv(yTCuY + lI), yTCuPu)
+            yTCuPu = tf.matmul(tf.matmul(yT, Cu), tf.expand_dims(self.P[u], axis=1))
+            Xu = tf.squeeze(tf.matmul(tf.linalg.inv(yTCuY + lI), yTCuPu), axis=1)
+            self.X[u].assign(Xu)
 
     def optimize_item(self):
         xT = tf.transpose(self.X)
@@ -41,49 +44,30 @@ class Model:
             Ci = tf.linalg.diag(self.C[:, i])
             xTCiX = xTx + tf.matmul(tf.matmul(xT, Ci - tf.eye(Ci.shape[1])), self.X)
             lI = self.lambda_var * tf.eye(self.nf)
-            xTCiPi = tf.matmul(tf.matmul(xT, Ci), self.P[:, i])
-            self.Y[i] = tf.matmul(tf.linalg.inv(xTCiX + lI), xTCiPi)
+            xTCiPi = tf.matmul(tf.matmul(xT, Ci), tf.expand_dims(self.P[:, i], axis=1))
+            Yi = tf.squeeze(tf.matmul(tf.linalg.inv(xTCiX + lI), xTCiPi), axis=1)
+            self.Y[i].assign(Yi)
 
-    # @tf.function
+    @tf.function
     def train_one_step(self):
         self.optimize_user()
         self.optimize_item()
 
     def train(self, epoch=15):
-        predict_errors, confidence_errors, regularization_list, total_losses = [], [], [], []
+        predict_errors, regularization_list, total_losses = [], [], []
         for i in range(epoch):
             self.train_one_step()
-            predict_error, confidence_error, regularization, total_loss = self.loss_function()
+            predict_error, regularization, total_loss = self.loss_function()
 
             predict_errors.append(predict_error)
-            confidence_errors.append(confidence_error)
             regularization_list.append(regularization)
             total_losses.append(total_loss)
 
-            print('------------------------step %d----------------------' % i)
-            print('predict error: %f' % predict_error)
-            print('confidence error: %f' % confidence_error)
-            print('regularization: %f' % regularization)
-            print('total loss: %f' % total_loss)
-        return predict_errors, confidence_errors, regularization_list, total_losses
+            tf.print('------------------------step %d----------------------' % i)
+            tf.print('predict error: %f' % predict_error)
+            tf.print('regularization: %f' % regularization)
+            tf.print('total loss: %f' % total_loss)
+        return predict_errors, regularization_list, total_losses
 
     def predict(self):
-        return tf.tensordot(self.X, self.Y, axes=1)
-
-
-u1 = np.array([0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0])
-u2 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
-u3 = np.array([0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 0])
-u4 = np.array([0, 3, 4, 0, 3, 0, 0, 2, 2, 0, 0])
-u5 = np.array([0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0])
-u6 = np.array([0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0])
-u7 = np.array([0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 5])
-u8 = np.array([0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 4])
-u9 = np.array([0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0])
-u10 = np.array([0, 0, 0, 3, 0, 0, 0, 0, 4, 5, 0])
-
-R = np.array([u1, u2, u3, u4, u5, u6, u7, u8, u9, u10], dtype=np.float32)
-
-model = Model(R)
-model.train()
-print(model.predict())
+        return tf.matmul(self.X, tf.transpose(self.Y))
