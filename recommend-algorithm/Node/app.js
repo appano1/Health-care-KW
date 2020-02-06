80832a77eee41c02556783c68128349059f08913
이승철 <thankfather@naver.com>
2020-02-06 18:26:52 +0900

    노드에서 선호도 배열을 받아서 파이썬쉘에서 실행되도록 변경했습니다.

--- recommend-algorithm/Node/app.js
+++ recommend-algorithm/Node/app.js
@@ -1,19 +1,31 @@
 const {PythonShell} = require('python-shell');
 
+list = [[0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 0],
+        [0, 3, 4, 0, 3, 0, 0, 2, 2, 0, 0],
+        [0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0],
+        [0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 5],
+        [0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 4],
+        [0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0],
+        [0, 0, 0, 3, 0, 0, 0, 0, 4, 5, 0]];
+userNumber = 3;
+
 const options = {
     mode: 'text',
     pythonPath: '/opt/anaconda3/envs/tf1/bin/python3',
     pythonOptions: ['-u'],
-    scriptPath: '',
-    args: ['value1', 'value2', 'value3']
+    scriptPath: '/Users/leeseungcheol/GitHub/Health-care-KW/recommend-algorithm/',
+    args: [list, userNumber]
 };
 
-PythonShell.run('/Users/leeseungcheol/GitHub/Health-care-KW/recommend-algorithm/recommend_test01.py',
+PythonShell.run('recommend_test01.py',
     options, function(err, result) {
         if(err) throw err;
 
+        // result.forEach(v => console.log(v));
         let result_recommendation = [];
-
         result.forEach(v => result_recommendation.push(Number(v)));
         console.log(result_recommendation);
     });
